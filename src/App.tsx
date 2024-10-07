99ab90faf2365fbb6615b756a3a5bf9df0bae0fa
Daniel Idoko <idokoidogwu@gmail.com>
2024-10-07 17:10:42 +0100

    Hero section complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Navbar from './components/nav/big-screen-nav';
+import React, { useState } from 'react';
 import NavContainer from './components/nav/nav-container';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/Home';
+
 
 function App() {
+  const [navHeight, setNavHeight] = useState<number>(0)
   return (
     <div className="App">
-      <NavContainer />
+      <NavContainer setNavHeight={setNavHeight}/>
+      <div style={{height: `${navHeight}px`}} className='space-height-nav'></div>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeContainer from '../container/Home';
+
+interface Props{
+}
+const HomePage: React.FC<Props> = ({}) => {
+
+  return (
+    <HomeContainer />
+  );
+}
+
+export default HomePage;

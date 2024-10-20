2cac86f3075686d562740e9382acc6b39a111729
Daniel Idoko <idokoidogwu@gmail.com>
2024-10-20 08:00:42 +0100

    complete login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavContainer from './components/nav/nav-container';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/Home';
 import PuppiesForSell from './pages/PuppiesForSell';
+import Login from './pages/Login';
 
 
 function App() {
   const [navHeight, setNavHeight] = useState<number>(0)
+  const [isPuppiesForSale, setIsPuppiesForSale] = useState(false);
+
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    console.log(currentPath)
+    if (currentPath === '/puppies-for-sale/') {
+      setIsPuppiesForSale(true);
+    } else {
+      setIsPuppiesForSale(false);
+    }
+  }, []); // Runs only on initial render
+
   return (
     <div className="App">
-      <NavContainer setNavHeight={setNavHeight}/>
+      <NavContainer setNavHeight={setNavHeight} isPuppiesForSale={isPuppiesForSale}/>
       <div style={{height: `${navHeight}px`}} className='space-height-nav'></div>
 
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/puppies-for-sale" element={<PuppiesForSell />} />
+          <Route path="/log-in" element={<Login />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import '../styles/login.css'
+
+interface Props{
+}
+const Login: React.FC<Props> = ({}) => {
+
+    useEffect(() =>{
+        document.title = "Log into your account | PuppySpot";
+    }, []);
+
+    const [isVisible, setIsvisible] = useState(false)
+
+  return (
+    <main className='authentication authentication__page log-in'>
+        <h2>Discover, learn about, and find your new puppy!</h2>
+
+        <form action="" className='js-form-validate authentication__container'>
+
+            <h3 className="login-title">Log in to PuppySpot</h3>
+
+            <div className="authentication__social">
+                <a className="button ghost" href="https://www.puppyspot.com/auth/google">
+                    <img src='/img/googl-logo.svg' style={{width: '1.8rem', height: '1.8rem'}}/>
+                <span>Continue with Google</span>
+                </a>
+                <a className="button blue-facebook" href="https://www.puppyspot.com/auth/facebook">
+                    <img src="https://www.puppyspot.com/assets/img/facebook-icon-white.svg"/>
+                    <span>Continue with Facebook</span>
+                </a>
+            </div>
+
+            <div className="authentication__divider">
+                <hr/><span>or</span>
+                <hr/>
+            </div>
+
+            <div className="error-container js-error-container">
+             <span></span>
+            </div>
+
+            <div className="input-wrapper outlined-input email">
+                <input 
+                    autoFocus={true}
+                    autoComplete="off"
+                    tabIndex={0}
+                    type="text"
+                    id="log-in__email"
+                    pattern="[^@]+@([a-zA-Z0-9\-]+\.)+[a-zA-Z0-9\-]+"
+                    name="email"
+                    data-name="Email Address"
+                    data-validate="required,email"
+                    placeholder=""
+                    data-tid="emailTextInput"
+                    className="floating-input"
+                    />
+                <label htmlFor="log-in__password" className="floating-label">Email</label>
+            </div>
+
+            <div className="input-wrapper outlined-input password">
+                <input
+                    autoComplete="off"
+                    tabIndex={0}
+                    id="log-in__password"
+                    placeholder=""
+                    type={isVisible ? 'text' : 'password'}
+                    name="password"
+                    data-name="Password"
+                    data-validate="required"
+                    className="floating-input"
+                />
+                   <label htmlFor="log-in__password" className="floating-label">Password</label>
+                <span className={isVisible ? "password-visibility js-show-password visible" : "password-visibility js-show-password" }  onClick={()=> setIsvisible(!isVisible)}></span>
+            </div>
+
+            <div className="remember-and-reset">
+                <div className="remember-me">
+                <input tabIndex={0} id="remember" type="checkbox" name="remember" checked={true}/>
+                <label htmlFor="remember">
+                <span>Remember me?</span>
+                </label>
+                </div>
+                <a className="hyperlink" href="https://www.puppyspot.com/forgot-password">Forgot password?</a>
+            </div>
+
+            <div className="cta-loader">
+                <div className="loading cta invisible">
+                    <picture className="">
+                    <img id="" alt="" className=" lazyloaded" data-cy="" data-src="https://www.puppyspot.com/assets/img/components/loader-cta.svg" loading="lazy" src="https://www.puppyspot.com/assets/img/components/loader-cta.svg"/>
+                    </picture>
+                </div>
+                <input tabIndex={0} type="submit" className="button login-button main js-submit" value="Log In" />
+            </div>
+
+            <p>Don't have an account? <a className="hyperlink" href="https://www.puppyspot.com/sign-up">Sign up</a></p>
+
+        </form>
+    
+    </main>
+  );
+}
+
+export default Login;

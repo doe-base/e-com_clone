29b316dbccb62458615199f480c8ea0e44902cab
Daniel Idoko <idokoidogwu@gmail.com>
2024-11-11 03:30:26 +0100

    Started Login functionality

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { firebase, db } from './lips/firebase.prod'; // add firebase to project
+import { FirebaseContext } from './context/firebase' // use FirebaseContext to pass firebase from here
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value={{ firebase, db }}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>
 );
 

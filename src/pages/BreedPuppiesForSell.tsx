5f7072a0be7a06cbfdaa4a7eaaf2536c46d5431f
Daniel Idoko <idokoidogwu@gmail.com>
2024-11-23 23:27:37 +0100

    Navbar ready

--- src/pages/BreedPuppiesForSell.tsx
+++ src/pages/BreedPuppiesForSell.tsx
@@ -3,7 +3,7 @@ import Footer from '../components/footer';
 import BreedPuppiesForSellContainer from '../container/BreedPuppiesForSell';
 import AuthenticationPopup from '../components/authentication-popups/AuthenticationPopup';
 import { useParams } from 'react-router-dom';
-import AllbreedList from '../data/all-breeds.json'
+import AllbreedList from '../data/transformed_breeds.json'
 
 interface Props{
 }
@@ -15,24 +15,22 @@ interface Breed {
   id: number;
   characterID: number;
 }
+function findBySlug(items: Breed[], idToMatch: string | undefined): Breed | undefined {
+  return items.find(item => item.slug === idToMatch);
+}
 const BreedPuppiesForSell: React.FC<Props> = ({}) => {
   const [breedObj, setBreedObj] = useState<Breed>()
   const { breedSlug } = useParams();
+  const [allBreed, setAllBreed] = useState(AllbreedList)
 
-  const [allBreed, setAllBreed] = useState(AllbreedList.breedList)
-  function findBySlug(items: Breed[], idToMatch: string | undefined): Breed | undefined {
-    return items.find(item => item.slug === idToMatch);
-  }
+
   useEffect(()=>{
     setBreedObj(findBySlug(allBreed, breedSlug))
   }, []);
-
-
-  // Costomise with breed name. Eg, Alika Puppies for Sale
-    useEffect(() =>{
-        document.title = `${breedObj?.name ? breedObj.name : ''} Puppies for Sale | PuppySpot`;
-        document.body.classList.add('gray-background')
-    }, [breedObj]);
+  useEffect(() =>{
+      document.title = `${breedObj?.name ? breedObj.name : ''} Puppies for Sale | PuppySpot`;
+      document.body.classList.add('gray-background')
+  }, [breedObj]);
 
   return (
     <div>
